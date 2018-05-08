1b766fa0ad0b7c9af6ef52d6f837d035506344d0
Colton815 <colton@getstone.io>
2018-05-08 12:22:23 -0700

    Initial Commit

---
+++ StackAndQueues/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StackAndQueues
+//
+//  Created by Colton Walker on 5/8/18.
+//  Copyright © 2018 Colton Walker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
